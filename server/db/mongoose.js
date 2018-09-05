31a9fa033eef5e867c13e84ee00e612acd00805d
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-09-05 10:06:16 +0600

    remove bootstrap. add mongoose error connection handler

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,8 @@ const port = 27017;
 mongoose.connect(`mongodb://localhost:${port}/blog`, { useNewUrlParser: true })
   .then(() => {
     console.log(`mongoose connected on port ${port}`)
+  }).catch((e) => {
+    console.log('unable to connect to mongoose ', e);
   })
   .catch(e => console.log(e));
 
